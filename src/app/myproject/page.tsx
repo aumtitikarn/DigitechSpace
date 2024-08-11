61c63438aa61298bcf47f84ccea259641a30ebbb
phornthi <fernphornthiwa906@gmail.com>
2024-08-11 14:47:16 +0700

    .

--- src/app/myproject/page.tsx
+++ src/app/myproject/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React from "react";
 import Link from "next/link";
 import { IoIosStar } from "react-icons/io";
@@ -62,30 +62,30 @@ const ReviewCard: React.FC<{ products: Product[]; showButton: boolean }> = ({
                 className="w-[150px] h-[90px] rounded-md object-cover mr-4"
               />
               <div className="flex flex-col flex-1 justify-between h-full">
-                <p className="text-lg font-semibold truncate sm:w-[190px] lg:w-[1200px]">
+                <p className="text-lg font-semibold truncate sm:text-md md:text-lg lg:text-xl">
                   {product.name}
                 </p>
                 <div className="flex items-center">
-                  <span className="text-gray-500 mr-2 text-2xl">
+                  <span className="text-gray-500 mr-2 text-xl sm:text-lg md:text-xl">
                     <MdAccountCircle />
                   </span>
-                  <p className="text-sm text-gray-600 truncate sm:w-[190px] lg:w-[1200px]">
+                  <p className="text-sm text-gray-600 truncate sm:text-xs md:text-sm lg:text-base">
                     {product.author}
                   </p>
                 </div>
                 <div className="flex items-center">
-                  <span className="text-yellow-500 mr-2">
+                  <span className="text-yellow-500 mr-2 text-lg sm:text-base md:text-lg">
                     <IoIosStar />
                   </span>
-                  <span className="text-sm text-gray-600">
+                  <span className="text-xs sm:text-xs md:text-sm lg:text-base">
                     {product.rating} ({product.reviews}) | Sold {product.sold}
                   </span>
                 </div>
-                <p className="text-lg font-bold text-[#33529B] mb-4">
+                <p className="text-base sm:text-sm md:text-lg lg:text-lg font-bold text-[#33529B] mb-4">
                   {product.price} THB
                 </p>
                 {showButton && (
-                  <div className="absolute bottom-4 lg:right-4 right-2">
+                  <div className="absolute bottom-2 lg:right-4 right-4 md:bottom-2">
                     <button className="bg-blue-600 text-white px-4 py-1 md:px-6 md:py-2 rounded-lg text-xs md:text-sm">
                       Check the project
                     </button>
@@ -123,7 +123,7 @@ const MyProject: React.FC = () => {
     <div className="flex flex-col min-h-screen bg-[#FBFBFB]">
       <main className="flex-grow">
         <Navbar session={session} />
-        <div className="lg:mx-64 lg:mt-10 lg:mb-10 mt-10 mb-10 mx-5">
+        <div className="lg:mx-64 lg:mt-10 lg:mb-10 mt-10 mb-10 mx-1">
           <div>
             <h2 className="font-bold mb-4 text-[24px]">Waiting to check</h2>
             <div className="review-list space-y-4 mb-6">
@@ -131,12 +131,12 @@ const MyProject: React.FC = () => {
             </div>
 
             <p className="text-sm text-gray-500 mb-8">
-              *Please inspect the product and press the "Check the project"button before 7 days.
+              *Please inspect the product and press the "Check the project" button before 7 days.
               If you do not press the button, you will not be able to file a
               complaint and refund.
             </p>
 
-            <h2 className="font-bold mb-4  text-[24px]">My project</h2>
+            <h2 className="font-bold mb-4 text-[24px]">My project</h2>
             <div className="review-list space-y-4">
               <ReviewCard products={userProducts} showButton={false} />
             </div>
