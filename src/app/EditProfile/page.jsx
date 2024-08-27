933f980454bac43ebe2d4c14cb39f49c9850c276
Tbar-Ghost <142732021+Tbar-Ghost@users.noreply.github.com>
2024-08-27 21:23:03 +0700

    -

--- src/app/EditProfile/page.jsx
+++ src/app/EditProfile/page.jsx
@@ -12,6 +12,7 @@ import { useSession } from "next-auth/react";
 import { FaLink } from "react-icons/fa";
 import Box from "next-auth/providers/box";
 import { MdAccountCircle } from "react-icons/md";
+import { FaPlus } from "react-icons/fa6";
 
 function page() {
   const { data: session, status } = useSession();
@@ -33,7 +34,18 @@ function page() {
         <div className="flex flex-col items-center w-full max-w-lg">
           
           <div className="flex flex-row justify-center">
-            <MdAccountCircle className="w-40 h-40 rounded-full"/>
+          <div className="relative">
+            <MdAccountCircle 
+            className="rounded-full text-gray-500" 
+            style={{ width: "95px", height: "95px" }} 
+            />
+        <div 
+          className="absolute right-0 bottom-0 bg-gray-500 rounded-full p-1" 
+          style={{ transform: "translate(25%, 25%)" }}
+          >
+        <FaPlus size={18} className="text-white" />
+        </div>
+        </div>
           </div>
 
           <div className="flex flex-row justify-center">
@@ -57,6 +69,22 @@ function page() {
             <input
               type="email"
               placeholder="Enter email"
+              className="w-full p-2 mb-4 border border-gray-300 rounded"
+            />
+            <div className="flex flex-row items-center w-full mt-4">
+            <p>Phone Number</p>
+            </div>
+            <input
+              type="text"
+              placeholder="Enter phone number"
+              className="w-full p-2 mb-4 border border-gray-300 rounded"
+            />
+            <div className="flex flex-row items-center w-full mt-4">
+            <p>Facebook</p>
+            </div>
+            <input
+              type="text"
+              placeholder="Enter your facebook"
               className="w-full p-2 mb-4 border border-gray-300 rounded"
             />
             <button
@@ -83,6 +111,22 @@ function page() {
             <p>Email</p>
             </div>
             <div className="w-full p-2 mb-4 text-zinc-400">{session?.user?.email}</div>
+            <div className="flex flex-row items-center w-full mt-4">
+            <p>Phone Number</p>
+            </div>
+            <input
+              type="text"
+              placeholder="Enter phone number"
+              className="w-full p-2 mb-4 border border-gray-300 rounded"
+            />
+            <div className="flex flex-row items-center w-full mt-4">
+            <p>Facebook</p>
+            </div>
+            <input
+              type="text"
+              placeholder="Enter your facebook"
+              className="w-full p-2 mb-4 border border-gray-300 rounded"
+            />
             <button
               onClick={handleSave}
               className="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-600"
