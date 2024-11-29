2112fe193841091ad59fb266289afb5a1c735638
Titikarn Waitayasuwan <aumtitikarn003@gmail.com>
2024-11-29 17:18:17 +0000

    Update page.jsx

--- src/app/Profile/ViewProfile/[id]/page.jsx
+++ src/app/Profile/ViewProfile/[id]/page.jsx
@@ -177,6 +177,7 @@ function Page({ params }) {
                   alt="Profile"
                   width={128}
                   height={128}
+                  quality={100}
                   className="rounded-full w-full h-full object-cover"
                   onError={() => setFailedImages((prev) => [...prev, imageSource])}
                 />
